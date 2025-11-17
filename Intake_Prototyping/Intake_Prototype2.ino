1f5a862faff87cfd2bd11fa2a19e10290c0f05f3
danielbritomatehuala <danielbritomatehuala@gmail.com>
2025-11-17 14:48:58 -0800

    Intake Code Prototype Added
    First two scripts of arduino code were added into a new folder yipee.

---
+++ Intake_Prototyping/Intake_Prototype2.ino
@@ -0,0 +1,84 @@
+/*
+  L298N H-Bridge Demo
+  l298-demo.ino
+
+Built off of code from:  
+  DroneBot Workshop 2022
+  https://dronebotworkshop.com
+
+  This code will loop between acceleartion and decceleration in one direction, and acceleration and decceleration in the other direction.
+*/
+ 
+// Motor Connections (ENA & ENB must use PWM pins)
+#define IN1 9
+#define IN2 8
+
+
+#define ENA 10
+
+ 
+void motorAccel() {
+  for (int i = 0; i < 256; i++) {
+    analogWrite(ENA, i);
+
+    delay(20);
+  }
+}
+ 
+void motorDecel() {
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(ENA, i);
+
+    delay(20);
+  }
+}
+ 
+void setup() {
+ 
+  // Set motor connections as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+ 
+ 
+  pinMode(ENA, OUTPUT);
+ 
+ 
+  // Start with motors off
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+
+ 
+  analogWrite(ENA, 0);
+
+}
+ 
+void loop() {
+ 
+  // Set motor forward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+ 
+  // Accelerate & decelerate both motors forward
+  motorAccel();
+
+  delay(1000);
+
+  motorDecel();
+ 
+  delay(500);
+ 
+  // Set motors reverse
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+ 
+  // Accelerate & decelerate both motors backward
+  motorAccel();
+
+  delay(1000);
+  motorDecel();
+ 
+  delay(500);
+ 
+}
